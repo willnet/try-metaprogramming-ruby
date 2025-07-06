e37b6ec0c428b40e1c715a38181c58b43fc7d8cb
Shinichi Maeshima <netwillnet@gmail.com>
2025-07-06 18:51:13 +0900

    Fix deploy

--- src/problems.js
+++ src/problems.js
@@ -21,7 +21,13 @@ import { problem as problem_04_block_01_block_first_step_q4 } from './problems/0
 import { problem as problem_04_block_02_evil_mailbox } from './problems/04_block_02_evil_mailbox.js';
 import { problem as problem_04_block_03_simple_bot } from './problems/04_block_03_simple_bot.js';
 import { problem as problem_05_class_definition_01_class_definition_first_step_q1 } from './problems/05_class_definition_01_class_definition_first_step_q1.js';
-import { problem as problem_05_class_definition_02_simple_mock } from './problems/05_class_definition_02_simple_mock.js';
+import { problem as problem_05_class_definition_01_singleton_methods } from './problems/05_class_definition_01_singleton_methods.js';
+import { problem as problem_05_class_definition_02_anonymous_classes } from './problems/05_class_definition_02_anonymous_classes.js';
+import { problem as problem_05_class_definition_03_meta_attr_accessor } from './problems/05_class_definition_03_meta_attr_accessor.js';
+import { problem as problem_05_class_definition_04_class_configuration } from './problems/05_class_definition_04_class_configuration.js';
+import { problem as problem_05_class_definition_05_method_wrapping } from './problems/05_class_definition_05_method_wrapping.js';
+import { problem as problem_05_class_definition_06_scope_gates } from './problems/05_class_definition_06_scope_gates.js';
+import { problem as problem_05_class_definition_07_simple_mock } from './problems/05_class_definition_07_simple_mock.js';
 import { problem as problem_06_codes_generate_codes_01_simple_model } from './problems/06_codes_generate_codes_01_simple_model.js';
 
 export const problems = [
@@ -47,6 +53,12 @@ export const problems = [
   problem_04_block_02_evil_mailbox,
   problem_04_block_03_simple_bot,
   problem_05_class_definition_01_class_definition_first_step_q1,
-  problem_05_class_definition_02_simple_mock,
+  problem_05_class_definition_01_singleton_methods,
+  problem_05_class_definition_02_anonymous_classes,
+  problem_05_class_definition_03_meta_attr_accessor,
+  problem_05_class_definition_04_class_configuration,
+  problem_05_class_definition_05_method_wrapping,
+  problem_05_class_definition_06_scope_gates,
+  problem_05_class_definition_07_simple_mock,
   problem_06_codes_generate_codes_01_simple_model
 ];
